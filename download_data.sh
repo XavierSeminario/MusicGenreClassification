1b538295c43f4ad1cf5acc890225c029f765be6b
PauFuentes <118255835+PauFuentes@users.noreply.github.com>
2023-05-20 23:03:49 +0200

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -6,7 +6,7 @@ cd data
 curl -O https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
 echo "f0df49ffe5f2a6008d7dc83c6915b31835dfe733  fma_metadata.zip" | sha1sum -c -
 
-unzip fma_metadata.zip
+7z e fma_metadata.zip
 
 mkdir audio
 cd audio
@@ -14,4 +14,4 @@ cd audio
 curl -O https://os.unil.cloud.switch.ch/fma/fma_small.zip
 echo "ade154f733639d52e35e32f5593efe5be76c6d70  fma_small.zip"    | sha1sum -c -
 
-unzip -q fma_small.zip
+7z e fma_small.zip
