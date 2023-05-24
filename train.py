0d8a91180a3e8b51e9917f885276ec43b6d3fa03
ManuIluro22 <manuelarnaufernandez@gmail.com>
2023-05-24 09:11:50 +0000

    Add train.py

---
+++ train.py
@@ -0,0 +1,21 @@
+import tqdm
+import numpy as np
+
+def train(model, device, train_loader, optimizer, criterion):
+    losses = []
+    model.train()
+    t = tqdm.tqdm(enumerate(train_loader), total=len(train_loader))
+    t.set_description('Train')
+    for batch_idx, (data, target) in t:
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad() #backpropagation
+        data = data.resize_(data.size()[0], 64,64).float()
+        output = model(data)
+        print(output, target)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+        t.set_postfix(loss=loss.item())
+
+    return np.mean(losses)
