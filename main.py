Fix Vanishing Gradient

main.py
@@ -24,8 +24,8 @@ torch.cuda.manual_seed_all(hash("so runs are repeatable") % 2**32 - 1)
 # Device configuration
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-epochs = 30
-batch_size = 50        # number of samples during training
+epochs = 50
+batch_size = 100        # number of samples during training
 test_batch_size = 50  # number of samples for test 
 train_size = 0.8
 
@@ -59,12 +59,13 @@ if __name__ == "__main__":
 
         spectrograms_list, genres_list = LoadDataPipeline()
 
-        train_dataloader,test_dataloader = CreateTrainTestLoaders(spectrograms_list, genres_list, train_size, 
+        
+        train_dataloader,test_dataloader, targets = CreateTrainTestLoaders(spectrograms_list, genres_list, train_size, 
                                                                 train_kwargs, test_kwargs)
         
-        print(test_dataloader)
         print("Creacion Modelo")
-        model = CNNGH()
+        model = CNNGH1D()
+        model.apply(init_weights)
         loss = nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
         #Scheduler that will modify the learning ratio dinamically according to the test loss
@@ -75,6 +76,7 @@ if __name__ == "__main__":
         for epoch in range(1, epochs + 1):
             loss_train_epoch = train(model, device, train_dataloader, optimizer, loss, epoch)
             loss_test_epoch, prediction = test(model, device, test_dataloader, loss)
+            
         #config = dict(
         #   epochs=5,
         #  classes=10,

models/models.py
@@ -107,26 +107,80 @@ class CNNGH(nn.Module):
             super(CNNGH, self).__init__()
             self.name="CNNGH"
             self.layer1 = nn.Sequential(
-                nn.Conv2d(in_channels=1,out_channels=64, kernel_size=(5)),
+                nn.Conv2d(in_channels=1,out_channels=16, kernel_size=(5),stride=2),
+                nn.Dropout(0.5),
                 nn.ReLU(inplace=True),
-                nn.BatchNorm2d(64),
+                nn.BatchNorm2d(16),
                 nn.MaxPool2d(2)
             )
             self.layer2 = nn.Sequential(
-                nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(5)),
+                nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(5),stride=2),
+                nn.Dropout(0.5),
                 nn.ReLU(inplace=True),
-                nn.BatchNorm2d(128),
+                nn.BatchNorm2d(32),
                 nn.MaxPool2d(2)
             )
-            self.fc1 = nn.Linear(in_features=642,out_features=64)
-            self.fc2 = nn.Linear(in_features=64, out_features=8)
+            self.fc1 = nn.Linear(in_features=17696,out_features=200)
+            self.drop = nn.Dropout(0.25)
+            self.fc2 = nn.Linear(in_features=200, out_features=8)
         
         def forward(self, x):
             x = x.unsqueeze(1)
             out = self.layer1(x)
             out = self.layer2(out)
+            out = out.view(out.size(0), -1)
             out = self.fc1(out)
+            out = self.drop(out)
             out = F.relu(out)
             out = self.fc2(out)
             out = F.relu(out)
-            return out.view(out.size(0), -1)
+            return out
+        
+
+class CNNGH1D(nn.Module):
+        def __init__(self):
+            super(CNNGH1D, self).__init__()
+            self.name="CNNGH1D"
+            self.layer1 = nn.Sequential(
+                nn.Conv1d(in_channels=128,out_channels=128, kernel_size=4),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.25),
+                nn.BatchNorm1d(128),
+                nn.MaxPool1d(4)
+            )
+            self.layer2 = nn.Sequential(
+                nn.Conv1d(in_channels=128, out_channels=128, kernel_size=4),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.25),
+                nn.BatchNorm1d(128),
+                nn.MaxPool1d(4)
+            )
+            self.layer3 = nn.Sequential(
+                nn.Conv1d(in_channels=128, out_channels=128, kernel_size=4),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.25),
+                nn.BatchNorm1d(128),
+                nn.MaxPool1d(2)
+            )
+
+            self.fc1 = nn.Linear(in_features=4864 ,out_features=8)
+            self.dropout = nn.Dropout(0.3)
+            
+                
+        def forward(self, x):
+            #x = x.reshape(100,128,1291)
+            out = self.layer1(x)
+            out = self.layer2(out)
+            out = self.layer3(out)
+            out = out.view(out.size(0), -1)
+            out = self.fc1(out)
+            out = F.relu(out)
+            out = self.dropout(out)
+            return out
+        
+def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear) or isinstance(m, nn.Conv1d):
+                nn.init.kaiming_uniform_(m.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)

test.py
@@ -7,7 +7,8 @@ import torch
 def test(model, device, test_loader, criterion):
     losses = []
     model.eval()
-    prediction = []
+    all_preds = []
+    
     t = tqdm.tqdm(enumerate(test_loader), total=len(test_loader))
     t.set_description('Test')
     with torch.no_grad():
@@ -19,7 +20,7 @@ def test(model, device, test_loader, criterion):
             loss = criterion(output, target)
             losses.append(loss.item())
             t.set_postfix(loss=loss.item())
-            prediction.append(predicted)
+            all_preds.extend(predicted.detach().cpu().numpy())
             total += target.size(0)
             correct += (predicted == target).sum().item()
 
@@ -27,4 +28,4 @@ def test(model, device, test_loader, criterion):
               f"test images: {correct / total:%}")
         
         wandb.log({"test_accuracy": correct / total})
-    return np.mean(losses), prediction
+    return np.mean(losses), all_preds

utils/utils.py
@@ -10,6 +10,7 @@ import librosa
 from torch.utils.data.dataloader import DataLoader, Dataset
 import tqdm
 from sklearn.model_selection import train_test_split
+from torchvision import transforms
 
 # Number of samples per 30s audio clip.
 SAMPLING_RATE = 44100
@@ -188,6 +189,13 @@ def LoadFixCSV():
 
 def CreateTrainTestLoaders(spectrograms_list, genres_list, train_size, train_kwargs, test_kwargs):
     #Faltaria afegir split de test i train 
+    train_mean = np.mean(spectrograms_list)/255. #Mean of all images
+    train_std = np.std(spectrograms_list)/255. 
+    
+    #transform = transforms.Compose([
+        #transforms.Normalize((train_mean,), (train_std,))
+        #])
+
     X_train, X_val, y_train, y_val = train_test_split(spectrograms_list, genres_list, train_size=train_size, stratify=genres_list)
 
     train_ds = CustomSpectrogramDataset(X_train, y_train)
@@ -197,7 +205,7 @@ def CreateTrainTestLoaders(spectrograms_list, genres_list, train_size, train_kwa
     test_dataloader = torch.utils.data.DataLoader(test_ds, **test_kwargs)
 
 
-    return train_dataloader, test_dataloader #i tambe el test_dataloader
+    return train_dataloader, test_dataloader, y_val #i tambe el test_dataloader
 
 
 def LoadDataPipeline():