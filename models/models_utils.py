34551b19d765c85488f9c83e07779ca290e01369
PauFuentes <android2002fuentes@gmail.com>
2023-05-28 01:05:24 +0200

    Petits canvis

--- models/models_utils.py
+++ models/models_utils.py
@@ -9,8 +9,9 @@ def init_weights(model, method = 'Kaiming'):
 
     Parameters:
     - model: Model that we want to initialize weights and biases.
-    - method: Method of initialization. Kaiming (Kaiming Uniform) and Xavier (Xavier Uniform) supported. By default it is Kaiming
+    - method: Method of initialization. Kaiming (Kaiming Uniform) and Xavier (Xavier Uniform) supported. By default Kaiming is the selected method.
     """
+
     for m in model.modules():
         if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv1d):
             if method == 'Kaiming':
@@ -25,11 +26,12 @@ def calcular_parametres_del_model(model):
     Function that returns the total number of parameters of the model.
     
     Parameters:
-    - model: model whose number of parameters we want to evaluate.
+    - model: Model whose number of parameters we want to evaluate.
 
     Return:
-    - pytorch_total_params: Nuumber of parameters (int)
+    - pytorch_total_params: Number of parameters (int).
     """
+
     pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print("# trainable parameters: {:,}".format(pytorch_total_params))
     return pytorch_total_params
@@ -40,11 +42,12 @@ def mostra_estructura_model_torchviz(model):
     Function that allows the visualization of the structure of the model as a diagram.
 
     Parameters:
-    - model: model whose structure we want to evaluate.
+    - model: Model whose structure we want to evaluate.
 
     Return:
     - The model diagram.
     """
+    
     from torchviz import make_dot
     test_input = torch.randn(1, 1, 28, 28)
     return make_dot(model(test_input), params=dict(model.named_parameters()))
